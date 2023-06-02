c45bf934e77832fa94d0d021aac18be6fc212c6d
Guilherme <degrans@gmail.com>
2023-06-01 22:31:53 -0300

    initial commit

---
+++ src/test/java/com/saturno/bolao/BolaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saturno.bolao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BolaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
